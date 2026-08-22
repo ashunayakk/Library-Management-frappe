99c02ba159d1eb22bde3270150b6187fd6e46b3a
Ashutosh Nayak <ashunayak6393@email.com>
2026-08-22 17:57:02 +0530

    Add library management fixtures

--- library_management/hooks.py
+++ library_management/hooks.py
@@ -234,3 +234,10 @@ scheduler_events = {
 # List of apps whose translatable strings should be excluded from this app's translations.
 # ignore_translatable_strings_from = []
 
+fixtures = [
+    "Custom Field",
+    "Property Setter",
+    "Client Script",
+    "Server Script",
+]
+
